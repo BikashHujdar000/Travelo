6cb6cca31bd461b6f868877b7205293ec2640319
Bikash <hujdarbikash000@gmail.com>
2023-01-13 21:47:26 +0545

    Commited By Bikash Hujdar Chaudhary

---
+++ app/src/main/java/com/example/travelo/Fragments/UserLoginFragment.kt
@@ -0,0 +1,23 @@
+package com.example.travelo.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.travelo.R
+
+
+class UserLoginFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_login, container, false)
+    }
+
+
+}
