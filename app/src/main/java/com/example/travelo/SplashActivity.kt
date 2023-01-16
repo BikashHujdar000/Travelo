8f4c5e021e38dd943c870a5d2ff197a225529b84
Bikash <hujdarbikash000@gmail.com>
2023-01-16 07:58:45 +0545

    thus

--- app/src/main/java/com/example/travelo/SplashActivity.kt
+++ app/src/main/java/com/example/travelo/SplashActivity.kt
@@ -1,11 +1,14 @@
 package com.example.travelo
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
 import android.widget.ProgressBar
 import com.github.ybq.android.spinkit.style.ThreeBounce
 
@@ -14,19 +17,27 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-//hidding action bar
-        supportActionBar?.hide()
-            // setting up loading
-        val progressBar = findViewById<View>(R.id.spin) as ProgressBar
-        val threeBounce = ThreeBounce()
-        progressBar.indeterminateDrawable =threeBounce
+        // hidding atatus bar
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
 
-        // setting up time
-        Handler(Looper.getMainLooper()).postDelayed(
-            Runnable {
+            val progressBar = findViewById<View>(R.id.spin) as ProgressBar
+            val threeBounce = ThreeBounce()
+            progressBar.indeterminateDrawable = threeBounce
 
-                startActivity(Intent(this, MainActivity::class.java))
-            }, 2000
-        )
+            // setting up time
+            Handler(Looper.getMainLooper()).postDelayed(
+                Runnable {
+
+                    startActivity(Intent(this, MainActivity::class.java))
+                }, 8000
+            )
+        }
     }
 }
