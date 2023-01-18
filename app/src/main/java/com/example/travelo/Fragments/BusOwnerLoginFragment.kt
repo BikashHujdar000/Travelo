25a7d09fe7284135a301afab3ab588c47de6a1e3
Bikash <hujdarbikash000@gmail.com>
2023-01-18 09:18:37 +0545

    this is commited

--- app/src/main/java/com/example/travelo/Fragments/BusOwnerLoginFragment.kt
+++ app/src/main/java/com/example/travelo/Fragments/BusOwnerLoginFragment.kt
@@ -5,17 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.travelo.R
+import com.example.travelo.databinding.FragmentBusOwnerLoginBinding
+import com.example.travelo.databinding.FragmentOwnerHomeBinding
+import com.example.travelo.databinding.FragmentUserLoginBinding
 
 
 class BusOwnerLoginFragment : Fragment() {
-
+    lateinit var  binding: FragmentBusOwnerLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentBusOwnerLoginBinding.inflate(layoutInflater,container,false)
+
+
+        binding.signInButton.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_busOwnerLoginFragment_to_ownerHomeFragment)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bus_owner_login, container, false)
+        return binding.root
     }
 
 
--- app/src/main/java/com/example/travelo/Fragments/choiceFragment.kt
+++ app/src/main/java/com/example/travelo/Fragments/choiceFragment.kt
@@ -1,10 +1,12 @@
 package com.example.travelo.Fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavAction
 import androidx.navigation.Navigation
 import com.example.travelo.R
 import com.example.travelo.databinding.FragmentChoiceBinding
@@ -13,6 +15,7 @@ import com.example.travelo.databinding.FragmentHomeBinding
 class choiceFragment : Fragment() {
     lateinit var binding: FragmentChoiceBinding
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,6 +28,9 @@ class choiceFragment : Fragment() {
         // Inflate the layout for this fragment
         binding.user.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_choiceFragment_to_userRegisterFragment)
+        }
+        binding.busOwner.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_choiceFragment_to_busOwnerLoginFragment)
         }
 
         return binding.root
