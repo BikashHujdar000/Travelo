fcd01d56eb8a591d69c8f65fd1f460958ac31892
Bikash <hujdarbikash000@gmail.com>
2023-01-22 22:45:54 +0545

    RecyclerView Setted

--- app/src/main/java/com/example/travelo/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/travelo/Fragments/HomeFragment.kt
@@ -1,17 +1,22 @@
 package com.example.travelo.Fragments
 
-import android.app.Activity
-import android.graphics.Color
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.OnScrollListener
+import com.example.travelo.Adapters.TestAdapterUsers
+import com.example.travelo.Models.TestModel
 import com.example.travelo.R
 import com.example.travelo.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 lateinit var  binding: FragmentHomeBinding
+lateinit var recyclerView:RecyclerView
 
 
     override fun onCreateView(
@@ -19,8 +24,46 @@ lateinit var  binding: FragmentHomeBinding
         savedInstanceState: Bundle?
     ): View? {
         binding= FragmentHomeBinding.inflate(layoutInflater,container,false)
+
+        recyclerView  =binding.userHomeRecyclerView
         //setting Status Bar
-        requireActivity().getWindow().setStatusBarColor(getResources().getColor(R.color.purple_200));
+        requireActivity().getWindow().setStatusBarColor(getResources().getColor(R.color.theme1));
+
+
+        val manualData = ArrayList<TestModel>()
+
+
+        manualData.add(TestModel("Nepali YataYat","1:00pm","3:00","direct","Dharan","Gaighat","Rs.400","200Km"))
+
+        manualData.add(TestModel("Nepali YataYat","1:00pm","3:00","direct","Dharan","Gaighat","Rs.400","200Km"))
+        manualData.add(TestModel("DudhKoshi Travel ","1:00pm","3:00","direct","Dharan","Gaighat","Rs.400","200Km"))
+        manualData.add(TestModel("Narsingh Travel","1:00pm","3:00","direct","Dharan","Gaighat","Rs.400","200Km"))
+        manualData.add(TestModel("MidNight Travel","1:00pm","3:00","direct","Dharan","Gaighat","Rs.400","200Km"))
+        manualData.add(TestModel("Ramro Travel","1:00pm","3:00","direct","Dharan","Gaighat","Rs.400","200Km"))
+
+       val adapter= TestAdapterUsers(requireContext(),manualData)
+         recyclerView.adapter=adapter
+        recyclerView.layoutManager= LinearLayoutManager(requireContext())
+     //recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+        //      override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+        //        super.onScrollStateChanged(recyclerView, newState)
+        //        val totalItemCount = recyclerView.layoutManager!!.itemCount
+        //        if (!imageRequester.isLoadingData && totalItemCount == lastVisibleItemPosition + 1) {
+        //          requestPhoto()
+        //        }
+        //      }
+        //    })
+
+        recyclerView.addOnScrollListener((object :RecyclerView.OnScrollListener(){
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+            }
+        }))
 
         return binding.root
     }
